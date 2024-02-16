5a156175457d2bb7d14703aa395fc9841febefd6
Sakthi <sakthikumarmdu@gmail.com>
2024-02-16 21:53:11 +0530

    Implement FeedCache test case

---
+++ EssentialFeed/EssentialFeedTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  EssentialFeedTests
+//
+//  Created by Sakthi Kumar on 16/02/24.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
+
